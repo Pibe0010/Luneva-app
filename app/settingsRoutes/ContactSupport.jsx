c5f910cbfd723020f0088dedc60ddf01ec88aa0f
Daniel Montero Castro <salamapatela@gmail.com>
2025-12-06 10:55:52 +0100

    Setting menu routes

---
+++ app/settingsRoutes/ContactSupport.jsx
@@ -0,0 +1,19 @@
+import Styled from "styled-components/native";
+
+export default function ContactSupport() {
+  return (
+    <Container>
+      <Texto>notis</Texto>
+    </Container>
+  );
+}
+
+const Container = Styled.View`
+    flex: 1;
+    justify-content: center;
+    align-items: center;
+     background-color: #0f0e0e;  
+`;
+const Texto = Styled.Text`
+    color: #ffffff;
+`;
