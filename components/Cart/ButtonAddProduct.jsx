ae4aa6e1ead0be5d48c8bdc3f7f2abee1f37a7ca
Daniel Montero Castro <salamapatela@gmail.com>
2025-12-27 09:24:40 +0100

    Message when adding product to cart

--- components/Cart/ButtonAddProduct.jsx
+++ components/Cart/ButtonAddProduct.jsx
@@ -1,14 +1,19 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useEffect, useState } from "react";
 import Styled from "styled-components/native";
 import { fetchCartAdd } from "../../hooks/cart.js";
+import { AlertMessage } from "./AlertMessage.jsx";
 
 export const ButtonAddProduct = ({ product }) => {
+  const [message, setMessage] = useState(false);
+
   const queryClient = useQueryClient();
 
   const { mutate, isLoading } = useMutation({
     mutationFn: ({ id, amount }) => fetchCartAdd(id, amount),
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ["cart"] });
+      setMessage(true);
     },
   });
 
@@ -19,22 +24,52 @@ export const ButtonAddProduct = ({ product }) => {
     });
   };
 
+  useEffect(() => {
+    if (!message) return;
+
+    const timer = setTimeout(() => {
+      setMessage(false);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, [message]);
+
   return (
-    <AddToCart onPress={handleAddToCart} disabled={isLoading}>
-      <ButtonTitle>{isLoading ? "Adding..." : "Add to cart"}</ButtonTitle>
-    </AddToCart>
+    <ButtomContainer>
+      <AddToCart onPress={handleAddToCart} disabled={isLoading}>
+        <ButtonTitle>{isLoading ? "Adding..." : "Add to cart"}</ButtonTitle>
+      </AddToCart>
+
+      {message && (
+        <MessageContainer>
+          <AlertMessage msj="Product added to cart" />
+        </MessageContainer>
+      )}
+    </ButtomContainer>
   );
 };
 
+const ButtomContainer = Styled.View`
+  position: relative;
+  width: 100%;
+`;
 const AddToCart = Styled.TouchableOpacity`
-    background-color: ${({ theme }) => theme.bgButtonCard};
-    padding: 5px;
-    border-radius: 10px;
-    width: 90%;
+  background-color: ${({ theme }) => theme.bgButtonCard};
+  padding: 10px;
+  border-radius: 10px;
+  width: 100%;
 `;
 const ButtonTitle = Styled.Text`
-    font-size: 16px;
-    font-weight: bold;
-    color: ${({ theme }) => theme.text};;
-    text-align: center;
+  font-size: 16px;
+  font-weight: bold;
+  color: ${({ theme }) => theme.text};
+  text-align: center;
+`;
+const MessageContainer = Styled.View`
+  position: absolute;
+  top: -600px;
+  left: 0;
+  width: 100%;
+  z-index: 999;
+  elevation: 999;
 `;
