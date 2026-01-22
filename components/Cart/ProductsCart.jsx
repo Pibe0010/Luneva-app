3629fa5069b7265ad1f256d18263b3cedc5b99a4
Daniel Montero Castro <salamapatela@gmail.com>
2026-01-22 08:54:38 +0100

    Cart adjustment and discount display added

--- components/Cart/ProductsCart.jsx
+++ components/Cart/ProductsCart.jsx
@@ -2,23 +2,10 @@ import Styled from "styled-components/native";
 import { ProductCartDetails } from "./ProductCartDetails.jsx";
 
 export function ProductCart({ products }) {
-  const grouped = Object.values(
-    products.reduce((acc, item) => {
-      if (acc[item.ID_product] === undefined) {
-        acc[item.ID_product] = { ...item };
-      } else {
-        acc[item.ID_product].products_amount += item.products_amount;
-      }
-      return acc;
-    }, {})
-  );
-
-  const renderItem = ({ item: grouped }) => <ProductCartDetails product={grouped} />;
-
   return (
     <ProductListCart
-      data={grouped}
-      renderItem={renderItem}
+      data={products}
+      renderItem={({ item }) => <ProductCartDetails product={item} />}
       keyExtractor={(item) => item.ID_product}
     />
   );
